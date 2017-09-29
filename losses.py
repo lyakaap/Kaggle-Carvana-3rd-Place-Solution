c76be1d705f79d6a3564b2ae9b7ccdf4f5a6a420
lyakaap <lyakaap@has-key.org>
2017-09-29 18:42:58 +0900

    Add files via upload
    Add scripts of network & loss functions

---
+++ losses.py
@@ -0,0 +1,55 @@
+from keras.losses import binary_crossentropy
+import keras.backend as K
+
+
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred = K.cast(y_pred, 'float32')
+    y_pred_f = K.cast(K.greater(K.flatten(y_pred), 0.5), 'float32')
+    intersection = y_true_f * y_pred_f
+    score = 2. * K.sum(intersection) / (K.sum(y_true_f) + K.sum(y_pred_f))
+    return score
+
+def dice_loss(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = y_true_f * y_pred_f
+    score = (2. * K.sum(intersection) + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return 1. - score
+
+def bce_dice_loss(y_true, y_pred):
+    return binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
+
+def bce_logdice_loss(y_true, y_pred):
+    return binary_crossentropy(y_true, y_pred) - K.log(1. - dice_loss(y_true, y_pred))
+
+def weighted_bce_loss(y_true, y_pred, weight):
+    epsilon = 1e-7
+    y_pred = K.clip(y_pred, epsilon, 1. - epsilon)
+    logit_y_pred = K.log(y_pred / (1. - y_pred))
+    loss = weight * (logit_y_pred * (1. - y_true) + 
+                     K.log(1. + K.exp(-K.abs(logit_y_pred))) + K.maximum(-logit_y_pred, 0.))
+    return K.sum(loss) / K.sum(weight)
+
+def weighted_dice_loss(y_true, y_pred, weight):
+    smooth = 1.
+    w, m1, m2 = weight, y_true, y_pred
+    intersection = (m1 * m2)
+    score = (2. * K.sum(w * intersection) + smooth) / (K.sum(w * m1) + K.sum(w * m2) + smooth)
+    loss = 1. - K.sum(score)
+    return loss
+
+def weighted_bce_dice_loss(y_true, y_pred):
+    y_true = K.cast(y_true, 'float32')
+    y_pred = K.cast(y_pred, 'float32')
+    # if we want to get same size of output, kernel size must be odd
+    averaged_mask = K.pool2d(
+            y_true, pool_size=(50, 50), strides=(1, 1), padding='same', pool_mode='avg')
+    weight = K.ones_like(averaged_mask)
+    w0 = K.sum(weight)
+    weight = 5. * K.exp(-5. * K.abs(averaged_mask - 0.5))
+    w1 = K.sum(weight)
+    weight *= (w0 / w1)
+    loss = weighted_bce_loss(y_true, y_pred, weight) + dice_loss(y_true, y_pred)
+    return loss
---
+++ unet_mdcb.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from keras.layers import Conv2D, MaxPooling2D, UpSampling2D
+from keras.layers import Input, add, concatenate
+from keras.models import Model
+from keras.optimizers import RMSprop
+
+from losses import binary_crossentropy, dice_loss, bce_dice_loss, dice_coef, weighted_bce_dice_loss
+
+def get_unet_MDCB(input_shape=(1920, 1280, 3), init_nb=44, lr=0.0001, loss=bce_dice_loss):
+    
+    inputs = Input(input_shape)
+    
+    down1 = Conv2D(init_nb, (3, 3), activation='relu', padding='same')(inputs)
+    down1 = Conv2D(init_nb, (3, 3), activation='relu', padding='same')(down1)
+    down1pool = MaxPooling2D((2, 2), strides=(2, 2))(down1)
+    
+    down2 = Conv2D(init_nb*2, (3, 3), activation='relu', padding='same')(down1pool)
+    down2 = Conv2D(init_nb*2, (3, 3), activation='relu', padding='same')(down2)
+    down2pool = MaxPooling2D((2, 2), strides=(2, 2))(down2)
+
+    down3 = Conv2D(init_nb*4, (3, 3), activation='relu', padding='same')(down2pool)
+    down3 = Conv2D(init_nb*4, (3, 3), activation='relu', padding='same')(down3)
+    down3pool = MaxPooling2D((2, 2), strides=(2, 2))(down3)
+    
+    # stacked dilated convolution
+    dilate1 = Conv2D(init_nb*8, (3, 3), activation='relu', padding='same')(down3pool)
+    dilate2 = Conv2D(init_nb*8, (3, 3), activation='relu', padding='same')(dilate1)
+    dilate3 = Conv2D(init_nb*8, (3, 3), activation='relu', padding='same')(dilate2)
+    dilate4 = Conv2D(init_nb*8, (3, 3), activation='relu', padding='same')(dilate3)
+    dilate5 = Conv2D(init_nb*8, (3, 3), activation='relu', padding='same')(dilate4)
+    dilate6 = Conv2D(init_nb*8, (3, 3), activation='relu', padding='same')(dilate5)
+    dilate_all_added = add([dilate1, dilate2, dilate3, dilate4, dilate5, dilate6])
+    
+    up3 = UpSampling2D((2, 2))(dilate_all_added)
+    up3 = Conv2D(init_nb*4, (3, 3), activation='relu', padding='same')(up3)
+    up3 = concatenate([down3, up3])
+    up3 = Conv2D(init_nb*4, (3, 3), activation='relu', padding='same')(up3)
+    up3 = Conv2D(init_nb*4, (3, 3), activation='relu', padding='same')(up3)
+
+    up2 = UpSampling2D((2, 2))(up3)
+    up2 = Conv2D(init_nb*2, (3, 3), activation='relu', padding='same')(up2)
+    up2 = concatenate([down2, up2])
+    up2 = Conv2D(init_nb*2, (3, 3), activation='relu', padding='same')(up2)
+    up2 = Conv2D(init_nb*2, (3, 3), activation='relu', padding='same')(up2)
+    
+    up1 = UpSampling2D((2, 2))(up2)
+    up1 = Conv2D(init_nb, (3, 3), activation='relu', padding='same')(up1)
+    up1 = concatenate([down1, up1])
+    up1 = Conv2D(init_nb, (3, 3), activation='relu', padding='same')(up1)
+    up1 = Conv2D(init_nb, (3, 3), activation='relu', padding='same')(up1)
+    
+    classify = Conv2D(1, (1, 1), activation='sigmoid')(up1)
+
+    model = Model(inputs=inputs, outputs=classify)
+
+    model.compile(optimizer=RMSprop(lr), loss=loss, metrics=[dice_coef])
+
+    return model
